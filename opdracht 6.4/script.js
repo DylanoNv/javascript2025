b1a6eb7aa78baa1416d5ef51a57b7fd276fcbbda
DylanoNv <9015493@student.zadkine.nl>
2025-11-20 15:18:36 +0100

    6.4

---
+++ opdracht 6.4/script.js
@@ -0,0 +1,20 @@
+// Oefening 1
+function calculate(a, b) {
+  let sum = a + b;
+  console.log("Oefening 1 - Antwoord:", sum);
+}
+calculate(1, 12);
+
+// Oefening 2
+function multiplyByFive(number) {
+  let result = number * 5;
+  console.log("Oefening 2 - Antwoord:", result);
+}
+multiplyByFive(7);
+
+// Oefening 3
+function calculateMinutes(seconds) {
+  let minutes = seconds / 60;
+  console.log("Oefening 3 - Antwoord:", minutes);
+}
+calculateMinutes(3600);
